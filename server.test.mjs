882504384d33770d53d3d69406fd7099d227c1ef
japneet <japneetsaini084@gmail.com>
2023-07-12 09:59:31 +0530

    testing pending

--- server.test.mjs
+++ server.test.mjs
@@ -1,6 +1,6 @@
-// Import the necessary modules and functions
 import request from 'supertest';
-import app from './server'; // Assuming you have exported the Express app from server.mjs
+import { expect } from 'chai';
+import app from './server.mjs';
 
 describe('User Registration and Login', () => {
   let userToken = '';
@@ -8,44 +8,64 @@ describe('User Registration and Login', () => {
   // Test the user registration endpoint
   it('should register a new user', async () => {
     const response = await request(app)
-      .post('/api/register')
+      .post('/register')
       .send({
-        name: 'Jimmy',
-        userName: 'jimmy',
-        email: 'jimmy@gmail.com',
+        name: 'John Doe',
+        userName: 'johndoe',
+        email: 'johndoe@example.com',
         password: 'password123',
       });
 
-    expect(response.status).toBe(200);
-    expect(response.body.message).toBe('Created new User');
-    expect(response.body.result.name).toBe('Jimmy');
-    expect(response.body.result.userName).toBe('jimmy');
-    expect(response.body.result.email).toBe('jimmy@gmail.com');
+    expect(response.status).to.equal(200);
+    expect(response.body.message).to.equal('Created new User');
+    expect(response.body.result.name).to.equal('John Doe');
+    expect(response.body.result.userName).to.equal('johndoe');
+    expect(response.body.result.email).to.equal('johndoe@example.com');
   });
 
-  // Test the user login
+  // Test the user login endpoint
   it('should log in a user and return a token', async () => {
     const response = await request(app)
-      .post('/api/login')
+      .post('/login')
       .send({
-        email: 'jimmy@gmail.com',
+        email: 'johndoe@example.com',
         password: 'password123',
       });
 
-    expect(response.status).toBe(200);
-    expect(response.body.message).toBe('Logged In');
-    expect(response.body.email).toBe('jimmy@gmail.com');
-    expect(response.body.token).toBeDefined();
+    expect(response.status).to.equal(200);
+    expect(response.body.message).to.equal('Logged In');
+    expect(response.body.email).to.equal('johndoe@example.com');
+    expect(response.body.token).to.exist;
 
-    userToken = response.body.token; 
+    userToken = response.body.token;
   });
 
-  it('should get the user profile', async () => {
+  // Test the getUser endpoint
+  it('should get all users', async () => {
     const response = await request(app)
-      .get('/api/getUser')
+      .get('/getUser')
       .set('Authorization', `Bearer ${userToken}`);
 
-    expect(response.status).toBe(200);
-    expect(response.body.length).toBeGreaterThan(0);
+    expect(response.status).to.equal(200);
+    expect(response.body).to.be.an('array');
+    expect(response.body.length).to.be.greaterThan(0);
+  });
+
+  // Test the addPost endpoint
+  it('should create a new post', async () => {
+    const response = await request(app)
+      .post('/addPost')
+      .set('Authorization', `Bearer ${userToken}`)
+      .send({
+        title: 'New Post',
+        content: 'This is a new post',
+        tags: ['#tag1', '#tag2'],
+      });
+
+    expect(response.status).to.equal(200);
+    expect(response.body.message).to.equal('New post created');
+    expect(response.body.result.title).to.equal('New Post');
+    expect(response.body.result.content).to.equal('This is a new post');
+    expect(response.body.result.tags).to.deep.equal(['#tag1', '#tag2']);
   });
 });
